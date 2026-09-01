03b8b58a8517ed8497ebdf5463b35c5ed033abc2
Nghia Tran <nghiatran912005@gmail.com>
2026-09-01 20:26:21 +0700

    initial be

---
+++ backend/NongSanNuNuBE/src/main/java/com/backend/NongSanNuNuBE/NongSanNuNuBeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.NongSanNuNuBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongSanNuNuBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NongSanNuNuBeApplication.class, args);
+	}
+
+}
